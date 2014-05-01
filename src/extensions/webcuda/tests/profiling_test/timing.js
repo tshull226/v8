87d02b2e408d592f2db55b9db0ae1f20eadd7d55
Tom Shull <shull1@illinois.edu>
2014-04-30 23:29:45 -0500

    working on final paper

--- src/extensions/webcuda/tests/profiling_test/timing.js
+++ src/extensions/webcuda/tests/profiling_test/timing.js
@@ -1,11 +1,6 @@
 
 print("timing started");
 %BeginTiming();
-var sum = 0;
-for(var i = 0; i < 100000; i++){
-	sum += i;
-}
-print("stopped timer");
 var value = %GetTiming();
 
 print("timer value (in microseconds)" + value);
